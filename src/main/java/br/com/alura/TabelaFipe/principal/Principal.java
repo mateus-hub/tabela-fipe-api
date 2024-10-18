109033a1c366e5c59edc0a31db0b680c6132b85c
mateus-hub <theusskt2018@gmail.com>
2024-10-18 16:09:50 -0300

    Modelando as classes da aplicacao

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -1,23 +1,29 @@
 package br.com.alura.TabelaFipe.principal;
 
+import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.model.Modelos;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
+import br.com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
     public void exibeMenu() {
         var menu = """
-                ***OPÇÕES***
-                Carro
-                Moto
-                Caminhão
-                                
-                Digite uma das opções para consultar:
-                """;
+               *** OPÇÕES ***
+               Carro
+               Moto
+               Caminhão
+               
+               Digite uma das opções para consulta: 
+               """;
 
         System.out.println(menu);
         var opcao = leitura.nextLine();
@@ -33,5 +39,21 @@ public class Principal {
 
         var json = consumo.obterDados(endereco);
         System.out.println(json);
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta: ");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
